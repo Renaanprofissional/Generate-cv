8eacc616d383f0a38e3400a6a8a03d87082c4079
Renaanprofissional <renaan.profissional@gmail.com>
2025-09-02 14:46:58 -0300

    fix: import home

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
 
